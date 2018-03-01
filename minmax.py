e16e54f7dbf962730beee057569ba1ed2f409670
rogerioicestacio <rogerio.ic.estacio@gmail.com>
2018-03-01 07:03:36 +0000

    solução do mininax OK

--- minmax.py
+++ minmax.py
@@ -4,59 +4,135 @@ import getopt
 from tic_tac_toe import print_board
 from tic_tac_toe import getsecondplayer
 
-def checkwinner(b):
-    winner = 'd'
-    if b[0] == b[1] == b[2]:
-        winner = b[0]
-    elif b[3] == b[4] == b[5]:
-        winner = b[3]
-    elif b[6] == b[7] == b[8]:
-        winner = b[6]
-    elif b[0] == b[3] == b[6]:
-        winner = b[0]
-    elif b[1] == b[4] == b[7]:
-        winner = b[1]
-    elif b[2] == b[5] == b[8]:
-        winner = b[2]
-    elif b[0] == b[4] == b[8]:
-        winner = b[0]
-    elif b[2] == b[4] == b[6]:
-        winner = b[2]
-    return winner
+def checkwinner(b, player):
+    if (b[0] == b[1] == b[2] == player) or \
+        (b[3] == b[4] == b[5] == player) or \
+        (b[6] == b[7] == b[8] == player) or \
+        (b[0] == b[3] == b[6] == player) or \
+        (b[1] == b[4] == b[7] == player) or \
+        (b[2] == b[5] == b[8] == player) or \
+        (b[0] == b[4] == b[8] == player) or \
+        (b[2] == b[4] == b[6] == player):
+            return True
+    else:
+        return False
+
+def checkspace(board,player):
+    if len([t for t in board if t==player]) == 2:
+        try:
+            nextmove = board.index('_')
+            return nextmove
+        except ValueError:
+            return -1
+    else:
+        return -1
+
+def checknextmovewinner(b,player):
+    for i in range(0,7,3):
+        b1 = b[i:i+3]
+        nextmove = checkspace(b1,player)
+        if nextmove >= 0:
+            return i+nextmove
+
+    for i in range(3):
+        b1 = [b[i],b[i+3],b[i+6]]
+        nextmove = checkspace(b1,player)
+        if nextmove >= 0:
+            m = [i,i+3,i+6]
+            return m[nextmove]
+
+    b1 = [b[0],b[4],b[8]]
+    nextmove = checkspace(b1,player)
+    if nextmove >= 0:
+        m = [0,4,8]
+        return m[nextmove]
+    else:
+        b1 = [b[2],b[4],b[6]]
+        nextmove = checkspace(b1,player)
+        if nextmove >= 0:
+            m = [2,4,6]
+            return m[nextmove]
+        else:
+            return -1
 
 def score(winner,maxp):
     if winner == maxp:
         return 1
-    elif winner == 'd':
+    elif winner == 'n':
         return 0
     else:
         return -1
 
 def solve_minmax(board,maxp,player,root):
     global deepth
-    solution = [0,0,0,0,0,0,0,0,0]
-    try:
+    deepth += 1
+
+    solution = []
+    for i,p in enumerate(board):
+        if p == '_':
+            b1 = []
+            b1.extend(board)
+            b1[i]=player
+            if checkwinner(b1, player):
+                if player == maxp:
+                    solution.append(1)
+                else:
+                    solution.append(-1)
+                return solution
+
+    solution = []
+    for i,p in enumerate(board):
+        if p == '_':
+            b1 = []
+            b1.extend(board)
+            b1[i]=player
+            solution_node = solve_minmax(b1,maxp,getsecondplayer(player),False)
+            if player != maxp:
+                solution.append(max(solution_node))
+            else:
+                solution.append(min(solution_node))
+
+    if len(solution) == 0: #draw
+        solution.append(0)
+    return solution
+    '''
+    nextmove = checknextmovewinner(board,player)
+    if nextmove >= 0: #winning move
+        b1 = []
+        b1.extend(board)
+        b1[nextmove] = player
+        solution[nextmove] = 1
+    else:
+        nextmove = checknextmovewinner(board,getsecondplayer(player))
+        if nextmove >= 0: #must go move
+            b1 = []
+            b1.extend(board)
+            b1[nextmove] = player
+            solution[nextmove] = 1
+        else:
         board.index('_')
         for i,p in enumerate(board):
             if p == '_':
                 b1 = []
                 b1.extend(board)
                 b1[i]=player
-                deepth += 1
-                solution[i] = solve_minmax(b1,maxp,getsecondplayer(player),False)
-            elif root:
-                solution[i] = p
-        if root:
-            return solution
-        elif player == maxp:
-            #print(solution)
-            return max(solution)
-        else:
-            return min(solution)
+                winner = checkwinner(b1)
+                if winner == maxp:
+                    solution[i] = 1
+                elif:
+                    solution[i] = solve_minmax(b1,maxp,getsecondplayer(player),False)
     except ValueError:
         #print_board(board)
         #print(score(checkwinner(board),maxp))
         return score(checkwinner(board),maxp)
+    if root:
+        return solution
+    elif player == maxp:
+        #print(solution)
+        return max(solution)
+    else:
+        return min(solution)
+    '''
 
 #print(score(checkwinner(['x','o','x','o','x','o','x','o','x']),'x'))
 #print(score(checkwinner(['x','o','x','o','x','o','x','o','x']),'o'))
@@ -64,19 +140,20 @@ def solve_minmax(board,maxp,player,root):
 #print(solve_minmax(['_','_','_','_','_','_','_','_','_'],'x','x',True))
 
 def picknextmove(board,result,maxp):
-    if len([ t for t in result if t == 0]) == 9: #all draw
-        board[4]=maxp
-    elif len([ t for t in result if t == 1]) == 9: #all wins
-        board[4]=maxp
-    elif len([ t for t in result if t == -1]) == 9: #all loses
-        board[4]=maxp
-    elif len([ t for t in result if t == 1]) > 0: #chance to win
-        pos = result.index(1)
-        board[pos]=maxp
-    elif len([ t for t in result if t == 0]) > 0: #chance to draw
-        pos = result.index(0)
-        board[pos]=maxp
-    return board
+    possiblemoves = [i for i,t in enumerate(board) if t=='_']
+    if len(possiblemoves) > 0:
+        for i,pos in enumerate(possiblemoves):
+            if result[i] == 1:
+                board[pos] = maxp
+                return board
+        for i,pos in enumerate(possiblemoves):
+            if result[i] == 0:
+                board[pos]= maxp
+                return board
+        board[possiblemoves[0]]= maxp
+        return board
+    else:
+        return board
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
@@ -102,12 +179,13 @@ if __name__ == "__main__":
             elif opt in ("-v", "--verbose"):
                 verbose=True
         if board != None:
+            if verbose:
+                print_board(board)
             ts = time.time()
             result = solve_minmax(board,maxp,first_player,True)
             if verbose:
-                print("time = %0.6f sec  deepth=%d"%((time.time() - ts),deepth))
-                print("first player = %s \n"%first_player)
-                print(result)
+                print("time = %0.6f sec  depth=%d"%((time.time() - ts),deepth))
+                print("first player = %s result=%r \n"%(first_player,result))
                 board = picknextmove(board,result,maxp)
                 print_board(board)
                 print(''.join(board))
